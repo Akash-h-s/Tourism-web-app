1e24720271961bfdc979e4ae634ba7687aa3285c
Akash-h-s <akash3arya@gmail.com>
2024-07-11 20:51:07 +0530

    Initial commit

---
+++ vehicles.js
@@ -0,0 +1,12 @@
+let book=document.querySelector(".book");
+book.addEventListener("click",()=>{window.location.href="book.htm"});
+let but=document.querySelector(".button11");
+        let body=document.querySelector("#bd");
+        but.addEventListener("click",()=>{
+            bd.innerText="";
+            bd.innerText="Thank you for visiting our Website...Visit again"
+            bd.style.fontSize="40px";
+            bd.style.textAlign="center";
+            bd.style.marginTop="20%";
+            
+        })
